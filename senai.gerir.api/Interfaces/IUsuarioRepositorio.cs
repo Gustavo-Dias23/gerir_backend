e9a2f53e59dbf3fe86e007f3864541dced24f709
Gustavo Da Silva Dias <47759989865@info.local>
2021-01-04 17:20:42 -0300

    Interface iniciada

---
+++ senai.gerir.api/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.gerir.api.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        Usuario Cadastrar(IUsuarioRepositorio usuario);
+
+        Usuario Logar
+    }
+}
